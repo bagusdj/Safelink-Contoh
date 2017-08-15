935ae3eb2280546b68ea12f5934702064f9b6b59
bagusdj <bagusdj245@gmail.com>
2017-08-15 20:47:47 +0700

    Update Contoha.js

--- Contoha.js
+++ Contoha.js
@@ -22,11 +22,11 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
 if(x=="1"){xxx="post6.html"}
-if(x=="1"){xxx="post5.html"}
-if(x=="2"){xxx="post4.html"}
-if(x=="3"){xxx="post3.html"}
-if(x=="4"){xxx="post2.html"}
-if(x=="5"){xxx="post1.html"}a_to_ve[i].href="https://safegan.blogspot.com/2017/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="post5.html"}
+if(x=="3"){xxx="post4.html"}
+if(x=="4"){xxx="post3.html"}
+if(x=="5"){xxx="post2.html"}
+if(x=="6"){xxx="post1.html"}a_to_ve[i].href="https://safegan.blogspot.com/2017/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
